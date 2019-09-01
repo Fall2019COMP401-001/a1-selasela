e038f00cbd3a3136e28bba21e0e85d3810969257
selasela <selasela@selasela-pc>
2019-08-31 23:23:06 -0400

    working code for novice (hopefully)

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,32 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+
+		int custNum = scan.nextInt();
+				
+		for (int i=0; i<custNum; i++) {
+			double total = 0;
+			
+			String firstIn = scan.next().charAt(0) + ". ";
+			String last = scan.next();
+			
+			int numOfItems = scan.nextInt();
+			for (int j=0; j<numOfItems; j++) {
+				int quantity = scan.nextInt();
+				@SuppressWarnings("unused")
+				String item = scan.next();
+				double howMuch = scan.nextDouble();
+				double sale = quantity*howMuch;
+				total += sale;
+			}
+			String listing = firstIn + last + ": " + total;
+			
+			System.out.println(listing);
+		}
+			
 		
+		
+		scan.close();
+		// close scanner
 	}
 }
