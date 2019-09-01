014e0da44bd4f744f629337786f6352d5bf7143f
selasela <selasela@selasela-pc>
2019-09-01 11:49:39 -0400

    most of jedi code with errors in it (line 24)

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,73 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int itemAmt = scan.nextInt();
+		
+		String[] storeItems = new String[itemAmt];
+		// make array for items
+		
+		for (int i=0; i<itemAmt; i++) {
+			storeItems[i] = scan.next();
+		}
+		// looping through the list to fill in above array
+		
+
+		int custNum = scan.nextInt();
+		// number of customers
+		
+		int[] cust = new int[itemAmt];
+		// array of how many customers bought a certain item
+		
+		double[] total = new double[itemAmt];
+		// array to track how many of each item was bought
+		
+		for (int i=0; i<custNum; i++) {
+			
+//			@SuppressWarnings("unused")
+//			String first = scan.next();
+//			@SuppressWarnings("unused")
+//			String last = scan.next();
+//			// scanning for first and last name
+//			
+			// cust[i] = first + " " + last;
+			// putting customer name into cust array
+
+			int itemTot = scan.nextInt();
+			// number of items per person
+			
+			for (int j=0; j<itemTot; j++) {
+				int quant = scan.nextInt();
+				String item = scan.next();
+				// identifying number of the item and name of the item
+				
+				//double spent = 0;
+				
+				for (int x=0; x<itemAmt; x++) {
+					if (item.compareTo(storeItems[x]) == 0) {
+						total[x]+=quant;
+						// adds count on item
+						cust[x]+=1;
+					}
+				}
+			
+			}
+		}
+		
+		
+		
+		for (int i=0; i<storeItems.length; i++) {
+			if(total[i]==0) {
+				System.out.println("No customers bought " + storeItems[i]);
+			} else {
+				System.out.println(cust[i] + " customers bought " + total[i] + storeItems[i]);
+			}
+		}
+		// for loop
+		// if else statement (in loop) to see what items were/were not bought
+		// print certain things in if/else blocks
+		
+	
+		
+		scan.close();
 	}
 }
